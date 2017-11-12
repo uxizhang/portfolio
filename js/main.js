ce7324cdd23e939c5ea0790f1281eaab1029a589
Xi Zhang <xzhang19@pratt.edu>
2017-11-12 13:45:28 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//hero text type animation
+$(function(){
+		$("#typed").typed({
+			strings: ["continually asking critical questions", "intuitively visualizing information", "iteratively moving things around"],
+			typeSpeed: 30,
+			loop: true,
+		  backDelay: 1200
+		});
+});
