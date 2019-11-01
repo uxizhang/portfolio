f08163016444f9c817cff920ae7ad64f55746692
uxizhang <31977375+uxizhang@users.noreply.github.com>
2019-11-01 17:06:03 -0400

    Delete main.js

--- js/main.js
+++
@@ -1,9 +0,0 @@
-//hero text type animation
-$(function(){
-		$("#typed").typed({
-			strings: ["continually asking critical questions", "intuitively visualizing information", "iteratively moving things around"],
-			typeSpeed: 30,
-			loop: true,
-		  backDelay: 1200
-		});
-});
